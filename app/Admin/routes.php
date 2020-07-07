2268bebfb2bd87e64e98527281c8d3aa43606284
Ninee <whuanxu@163.com>
2020-07-08 05:27:18 +0800

    删除默认控制器

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -9,8 +9,6 @@ Route::group([
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
-
-    $router->get('/', 'HomeController@index');
     $router->resource('convenience_categories', 'ConvenienceCategoryController');
     $router->resource('convenience_info', 'ConvenienceInfoController');
     $router->get('convenience_posters/{info_id}', 'PosterController@convenienceInfo');
