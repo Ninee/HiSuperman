7d75d5846bce42f7403f0efd6ef6931c829ae1be
Ninee <whuanxu@163.com>
2020-07-10 03:33:13 +0800

    修复换行显示问题

--- app/Admin/Controllers/ConvenienceInfoController.php
+++ app/Admin/Controllers/ConvenienceInfoController.php
@@ -103,7 +103,7 @@ class ConvenienceInfoController extends Controller
         $grid->id('Id');
 //        $grid->user_id('用户');
         $grid->column('content', '内容')->display(function ($content) {
-            return strip_tags($content);
+            return nl2br($content);
         });
         $grid->pictures('图片')->gallery(['width' => 50]);
         $grid->copyer('复制次数');
