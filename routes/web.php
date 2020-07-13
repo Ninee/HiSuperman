115f016a7d497c7fd3c9e88d6d8b18836ebbc53d
Ninee <whuanxu@163.com>
2020-07-14 02:20:28 +0800

    增加默认跳转

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function () {
+    return redirect(route('admin.home'));
+});
 Route::group(['prefix' => 'tools'], function () {
     Route::get('/image_generator', function () {
        return view('tools.image_generator');
